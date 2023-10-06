4667fed0d8185198d50e6601523dadce8c9d25b3
Mikelonia <onajitemike4real@gmail.com>
2023-10-06 12:17:16 +0000

    Removal of trailing whitespace

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -3,7 +3,10 @@
 '''
 from typing import Callable
 
-
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     '''Creates a multiplier function.
     '''
+    def multiplier_function(x: float) -> float:
+        return x * multiplier
+
+    return multiplier_function
