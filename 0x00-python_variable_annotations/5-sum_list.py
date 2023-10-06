ce9239a042e86e80f1164abfc5f3aca8aae4a73b
Mikelonia <onajitemike4real@gmail.com>
2023-10-06 11:49:51 +0000

    Task 5

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
-'''Task 5's module. By Okpako Michael
+'''Task 5's module.
 '''
 from typing import List
-
 
 def sum_list(input_list: List[float]) -> float:
     '''Computes the sum of a list of floating-point numbers.
     '''
+    return sum(input_list)
+
+# Example usage:
+result: float = sum_list([1.5, 2.0, 3.5])
+print(result)
+
