25458d10a614543e0afb18b18f46a1f67b404a76
Mikelonia <onajitemike4real@gmail.com>
2023-10-06 11:52:02 +0000

    Task 5

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -7,8 +7,3 @@ def sum_list(input_list: List[float]) -> float:
     '''Computes the sum of a list of floating-point numbers.
     '''
     return sum(input_list)
-
-# Example usage:
-result: float = sum_list([1.5, 2.0, 3.5])
-print(result)
-
